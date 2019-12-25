7cc3643cd57a323f9f4a450c506970d7202c0c7d
David031 <davidtsang031@gmail.com>
2019-12-25 18:05:29 +0800

    Add scripts

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManager;
+using UnityEngine.UI;
+public class Menu : MonoBehaviour
+{
+    public void gameStart(){
+        SceneManager.LoadScene(1);
+    }
+    public void gameQuit(){
+        Application.Quit();
+    }
+}
