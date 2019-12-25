f71b665a15b0eea2449fbef526a3a79e75f7bfd8
David031 <davidtsang031@gmail.com>
2019-12-26 01:31:53 +0800

    Add Enemy Move

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
-{
+public class Enemy : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    public GameObject player;
+    void Start () {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+        Quaternion targetRotation = Quaternion.LookRotation (player.transform.position - transform.position);
+        transform.rotation = Quaternion.Slerp (transform.rotation, targetRotation, 1 * Time.deltaTime);
+        transform.position += transform.forward * 1f * Time.deltaTime;
     }
 }
